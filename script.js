aac27b1c3da21ac342511c76adfbeea46f1894bb
Darker Studios <114411364+drklegend0@users.noreply.github.com>
2025-01-15 20:30:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,25 +7,43 @@ fetch('apps/appmanager.json')
         data.apps.forEach(app => {
             // Create app card
             const card = document.createElement('div');
+            
+
+            
             card.className = 'app-card';
 
             // Add app icon
             const img = document.createElement('img');
+            const icon = document.createElement('img');
+
+            icon.id = "category"
+            icon.src = `images/icons/${app.icon}.svg`;
+            icon.alt = "category Icon";
+            
+            
             img.src = `images/${app.name.toLowerCase()}.png`;
-            img.alt = `${app.name} Icon`;
+            img.alt = `${app.name} Logo`;
 
             // Add app name
             const name = document.createElement('h3');
             name.textContent = app.name;
+            
+            const downloadIcon = document.createElement("img")
+            
+            downloadIcon.src = "images/icons/download.svg"
+            
+            downloadIcon.style = "width: 15px;     width: 48px; height: 48px;";
 
             // Add download link
             const downloadLink = document.createElement('a');
             downloadLink.href = `downloadpage.html?app=${encodeURIComponent(app.name)}`;
-            downloadLink.textContent = 'Download';
+            downloadLink.append(downloadIcon);
             downloadLink.className = 'download-button';
 
             // Append elements to card
             card.appendChild(img);
+            card.append(document.createElement("br"));
+            card.appendChild(icon);
             card.appendChild(name);
             card.appendChild(downloadLink);
 
